48249e3dc4b4fe150e1a2b19206fedef43c88ccd
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2022-02-14 16:21:32 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/nelioalves/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/nelioalves/workshopmongo/WorkshopmongoApplication.java
@@ -1,12 +1,33 @@
 package com.nelioalves.workshopmongo;
 
+import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class WorkshopmongoApplication {
+public class WorkshopmongoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 	}
 }
