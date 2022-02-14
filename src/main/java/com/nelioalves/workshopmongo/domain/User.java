1ce361c35ca251ce17fa6603fc2ed0587d7c03b3
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2022-02-14 16:27:54 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -7,7 +7,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
 public class User implements Serializable {
-	private static final long serialVersionUID = 1L;
 	
 	@Id
 	private String id;
