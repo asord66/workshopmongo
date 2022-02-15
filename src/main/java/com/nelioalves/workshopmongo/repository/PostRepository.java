8a5a6a2964923a0c3a4be53d9347e5ce1749c34f
Alex Rodrigues <alex.rodrigues23997@gmail.com>
2022-02-15 15:48:19 -0300

    Consulta simples com @Query

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -2,12 +2,19 @@ package com.nelioalves.workshopmongo.repository;
 
 import com.nelioalves.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    //ambos os métodos fazem a mesma coisa.
+    //dar preferência para a nomenclatura do spring (QUERY METHODS)
+    //para testar, trocar na chamada do método no service.
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
